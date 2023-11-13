7327e5595615b3c25005b6099662445db669739a
Humayun Kabir <kabirhumayun5902@gmail.com>
2023-11-13 21:20:24 +0600

    Simple class remove

--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -39,7 +39,7 @@
         </div>
         <!-- Main content -->
         <section class="content">
-            <div class="container-fluid">
+            <div class="">
                 <!-- Info boxes -->
                 <div class="row">
                     <div class="col-12 col-sm-6 col-md-4">
