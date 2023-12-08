e9b11c82722e9d6a7ab6ad3ef10b4f05d7e9a1ec
encoderit-ridoy <ridoy@encoderit.net>
2023-12-08 12:57:58 +0600

    dashboard completed

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,7 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\CollectRequest;
+use App\Models\Order;
 use App\Models\Payload;
+use App\Models\Payment;
+use App\Models\PaymentTransfer;
+use App\Models\User;
+use App\Models\Withdraw;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 
@@ -10,7 +17,22 @@ class DashboardController extends Controller
 {
     public function getDashboard()
     {
-        return view('dashboard.dashboard');
+        $users = User::count();
+        $paymentRequests = Payment::where('status', 'pending')->count();
+        $orders = Order::whereMonth('created_at', Carbon::now()->month)->count();
+        $collectRequest = CollectRequest::where('status', 'pending')->count();
+        $withdraws = Withdraw::where('status', 'pending')->count();
+        $transfers = PaymentTransfer::whereMonth('created_at', Carbon::now()->month)->where('status', '!=', 'failed')->count();
+        $transferData = PaymentTransfer::whereMonth('created_at', Carbon::now()->month)->get();
+        $data = [
+            'users' => $users,
+            'paymentRequests' => $paymentRequests,
+            'orders' => $orders,
+            'collectRequest' => $collectRequest,
+            'withdraws' => $withdraws,
+            'transfers' => $transfers,
+        ];
+        return view('dashboard.dashboard', compact('data', 'transferData'));
     }
 
     public function getPayloadData(Request $request)
--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -6,113 +6,124 @@
 @endsection
 
 @section('content_title')
-<h4 class="mt-2">Dashboard</h4>
-<div id="flash_message"></div>
+    <h4 class="mt-2">Dashboard</h4>
+    <div id="flash_message"></div>
 @endsection
 
 
 @section('main_content')
-<div class="row  page-content">
-    <!-- Main content -->
-    <section class="content">
-        <div class="dashboard_page">
-            <div class="row">
-                <div class="col-12 col-sm-6 col-md-4">
-                    <div class="info-box">
-                        <div class="img_col">
-                            <i class="fa-solid fa-users"></i>
-                        </div>
-                        <div class="card_text">
-                            <h3>Total Users</h3>
-                            <p><span>150</span>k</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-12 col-sm-6 col-md-4">
-                    <div class="info-box">
-                        <div class="img_col">
-                            <i class="fa-solid fa-money-check-dollar"></i>
-                        </div>
-                        <div class="card_text">
-                            <h3>Total Payment</h3>
-                            <p><span>100</span>k</p>
+    <div class="row  page-content">
+        <!-- Main content -->
+        <section class="content">
+            <div class="dashboard_page">
+                <div class="row">
+                    <div class="col-12 col-sm-6 col-md-4">
+                        <div class="info-box">
+                            <div class="img_col">
+                                <i class="fa-solid fa-users"></i>
+                            </div>
+                            <div class="card_text">
+                                <h3>Total Users</h3>
+                                <p><span>{{ $data['users'] }}</span></p>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="col-12 col-sm-6 col-md-4">
-                    <div class="info-box">
-                        <div class="img_col">
-                            <i class="fa-solid fa-cart-shopping"></i>
-                        </div>
-                        <div class="card_text">
-                            <h3>Total Orders</h3>
-                            <p><span>05</span></p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-12 col-sm-6 col-md-4">
-                    <div class="info-box">
-                        <div class="img_col">
-                            <i class="fa-regular fa-money-bill-1"></i>
-                        </div>
-                        <div class="card_text">
-                            <h3>Collect Request</h3>
-                            <p><span>75</span></p>
+                    <div class="col-12 col-sm-6 col-md-4">
+                        <div class="info-box">
+                            <div class="img_col">
+                                <i class="fa-solid fa-money-check-dollar"></i>
+                            </div>
+                            <div class="card_text">
+                                <h3>Pending Payment Request</h3>
+                                <p><span>{{ $data['paymentRequests'] }}</span></p>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="col-12 col-sm-6 col-md-4">
-                    <div class="info-box">
-                        <div class="img_col">
-                            <i class="fa-solid fa-landmark"></i>
-                        </div>
-                        <div class="card_text">
-                            <h3>Withdraws</h3>
-                            <p><span>725</span></p>
+                    <div class="col-12 col-sm-6 col-md-4">
+                        <div class="info-box">
+                            <div class="img_col">
+                                <i class="fa-solid fa-cart-shopping"></i>
+                            </div>
+                            <div class="card_text">
+                                <h3>Order of this month</h3>
+                                <p><span>{{ $data['orders'] }}</span></p>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="col-12 col-sm-6 col-md-4">
-                    <div class="info-box">
-                        <div class="img_col">
-                            <i class="fa-solid fa-money-bill-transfer"></i>
+                    <div class="col-12 col-sm-6 col-md-4">
+                        <div class="info-box">
+                            <div class="img_col">
+                                <i class="fa-regular fa-money-bill-1"></i>
+                            </div>
+                            <div class="card_text">
+                                <h3>Pending Collect Request</h3>
+                                <p><span>{{ $data['collectRequest'] }}</span></p>
+                            </div>
                         </div>
-                        <div class="card_text">
-                            <h3>Transfer</h3>
-                            <p><span>725</span></p>
+                    </div>
+                    <div class="col-12 col-sm-6 col-md-4">
+                        <div class="info-box">
+                            <div class="img_col">
+                                <i class="fa-solid fa-landmark"></i>
+                            </div>
+                            <div class="card_text">
+                                <h3>Pending Withdraws</h3>
+                                <p><span>{{ $data['withdraws'] }}</span></p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 col-sm-6 col-md-4">
+                        <div class="info-box">
+                            <div class="img_col">
+                                <i class="fa-solid fa-money-bill-transfer"></i>
+                            </div>
+                            <div class="card_text">
+                                <h3>Transfers In this month</h3>
+                                <p><span>{{ $data['transfers'] }}</span></p>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <div class="card card-default edit__inner__container">
-            <div class="card-body table-responsive">
-                <table class="table dataTable no-footer" id="table_id">
-                    <thead>
-                        <tr>
-                            <th scope="col">Id</th>
-                            <th scope="col">Subject</th>
-                            <th scope="col">Message</th>
-                            <th scope="col">ACTION</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>5052</td>
-                            <td>gsdgs</td>
-                            <td>dsfsdf</td>
-                            <td>
-                                <span class="completed">Completed</span>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
+            <h4 class="mt-4">Transfer Data of This month</h4>
+            <div class="card card-default edit__inner__container">
+                <div class="card-body table-responsive">
+                    <table class="table dataTable no-footer" id="table_id">
+                        <thead>
+                            <tr>
+                                <th scope="col">Date</th>
+                                <th scope="col">Sender Name</th>
+                                <th scope="col">Sender ID</th>
+                                <th scope="col">Recipient</th>
+                                <th scope="col">Recipient ID</th>
+                                <th scope="col">AMOUNT</th>
+                                <th scope="col">STATUS</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($transferData as $row)
+                                <tr>
+                                    <td>{{ date('d-M, Y', strtotime($row->created_at)) }}</td>
+                                    <td>{{ $row->sender->name }}</td>
+                                    <td>{{ $row->sender->master_id }}</td>
+                                    <td>{{ $row->receiver->name }}</td>
+                                    <td>{{ $row->receiver->master_id }}</td>
+                                    <td>{{ $row->amount }}</td>
+                                    @if ($row->status == 'success')
+                                        <td><span class="success">Success</span></td>
+                                    @else
+                                        <td><span class="pending">Failed</span></td>
+                                    @endif
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
             </div>
-        </div>
-    </section>
-    <!-- /.content -->
-    {{-- @include('employees.employee-search-modal') --}}
-</div>
+        </section>
+        <!-- /.content -->
+        {{-- @include('employees.employee-search-modal') --}}
+    </div>
 @endsection
 
 @section('script')
