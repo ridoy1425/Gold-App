f7a23a771e3c4243b671366756e088623e1a26a4
encoderit-ridoy <ridoy@encoderit.net>
2023-12-08 20:26:17 +0600

    message template updated

--- resources/views/message/template-create.blade.php
+++ resources/views/message/template-create.blade.php
@@ -31,57 +31,58 @@
                                     </div>
                                 </div>
                                 <div class="site-card-body">
-                                    @if (isset($template))
-                                        <form action="{{ url('message/template/update') }}" method="post"
-                                            enctype="multipart/form-data">
-                                        @else
-                                            <form action="{{ url('message/template/create') }}" method="post"
-                                                enctype="multipart/form-data">
-                                    @endif
-                                    @csrf
-                                    <div class="site-input-groups row">
-                                        <label for="" class="col-sm-3 col-label">Subject
-                                        </label>
-                                        <div class="col-sm-9">
-                                            <input type="text" name="subject" class="box-input">
-                                        </div>
-                                    </div>
-                                    <div class="site-input-groups row">
-                                        <label for="" class="col-sm-3 col-label">Message Body
-                                        </label>
-                                        <div class="col-sm-9">
-                                            <textarea name="message" class="form-textarea" cols="30" rows="8"></textarea>
-                                        </div>
-                                    </div>
+                                    {{-- @if (isset($template)) --}}
+                                    <form action="{{ url('message/template/update') }}" method="post">
+                                        @csrf
 
-                                    <div class="row site-input-groups">
-                                        <label for="" class="col-sm-3 col-label pt-0">Template Status
-                                        </label>
-                                        <div class="col-sm-5">
-                                            <div class="site-input-groups mb-0">
-                                                <div class="switch-field mb-0">
-                                                    <input type="radio" id="template_status_enable" name="status"
-                                                        value="enable" <?php if (isset($template)) {
-                                                            echo $template->status == 'enable' ? 'checked' : '';
-                                                        } else {
-                                                            echo 'checked';
-                                                        } ?>>
-                                                    <label for="template_status_enable">Enable</label>
-                                                    <input type="radio" id="template_status_disable" name="status"
-                                                        value="disable" <?php if (isset($template)) {
-                                                            echo $template->status == 'disable' ? 'checked' : '';
-                                                        } ?>>
-                                                    <label for="template_status_disable">Disable</label>
+                                        {{-- @else
+                                            <form action="{{ url('message/template/create') }}" method="post">
+                                                @csrf --}}
+                                        {{-- @endif --}}
+                                        <div class="site-input-groups row">
+                                            <label for="" class="col-sm-3 col-label">Subject
+                                            </label>
+                                            <div class="col-sm-9">
+                                                <input type="text" name="subject" value="{{ $template->subject ?? '' }}"
+                                                    class="box-input">
+                                            </div>
+                                        </div>
+                                        <div class="site-input-groups row">
+                                            <label for="" class="col-sm-3 col-label">Message Body
+                                            </label>
+                                            <div class="col-sm-9">
+                                                <textarea name="message" class="form-textarea" cols="30" rows="8">{{ $template->message ?? '' }}</textarea>
+                                            </div>
+                                        </div>
+
+                                        <div class="row site-input-groups">
+                                            <label for="" class="col-sm-3 col-label pt-0">Template Status
+                                            </label>
+                                            <div class="col-sm-5">
+                                                <div class="site-input-groups mb-0">
+                                                    <div class="switch-field mb-0">
+                                                        <input type="radio" id="template_status_enable" name="status"
+                                                            value="enable" <?php if (isset($template)) {
+                                                                echo $template->status == 'enable' ? 'checked' : '';
+                                                            } else {
+                                                                echo 'checked';
+                                                            } ?>>
+                                                        <label for="template_status_enable">Enable</label>
+                                                        <input type="radio" id="template_status_disable" name="status"
+                                                            value="disable" <?php if (isset($template)) {
+                                                                echo $template->status == 'disable' ? 'checked' : '';
+                                                            } ?>>
+                                                        <label for="template_status_disable">Disable</label>
+                                                    </div>
                                                 </div>
                                             </div>
                                         </div>
-                                    </div>
-                                    <div class="row">
-                                        <div class="offset-sm-3 col-sm-9">
-                                            <button type="submit" class="site-btn-sm primary-btn w-100">Send Message
-                                            </button>
+                                        <div class="row">
+                                            <div class="offset-sm-3 col-sm-9">
+                                                <button type="submit" class="site-btn-sm primary-btn w-100">Send Message
+                                                </button>
+                                            </div>
                                         </div>
-                                    </div>
                                     </form>
                                 </div>
                             </div>
