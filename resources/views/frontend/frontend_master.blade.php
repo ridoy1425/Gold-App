aca6b20ade1143fd6d73ddf3cc4435dcb7ef52a0
Ashik9295 <ashikur0112@gmail.com>
2023-12-14 17:49:47 +0600

    admin panel frontend complate

--- resources/views/frontend/frontend_master.blade.php
+++ resources/views/frontend/frontend_master.blade.php
@@ -6,19 +6,19 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<meta name="description" content="">
 	<meta name="author" content="">
-	<link rel="icon" href="{{ url('ui/frontend_assets') }}/img/favicons/favicon.ico">
+	<link rel="icon" href="{{ url('ui/frontend_assets/img/favicons/favicon.ico') }}">
 
 	<title>World Shine</title>
 
 	<link rel="preconnect" href="https://fonts.gstatic.com">
 	<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap" rel="stylesheet">
 
-	<link rel="stylesheet" href="{{ url('ui/frontend_assets') }}/css/bootstrap.min.css">
-	<link rel="stylesheet" href="{{ url('ui/frontend_assets') }}/css/tiny-slider.css">
-	<link rel="stylesheet" href="{{ url('ui/frontend_assets') }}/css/meanmenu.css">
-	<link rel="stylesheet" href="{{ url('ui/frontend_assets') }}/css/fontawesome.min.css">
-	<link rel="stylesheet" href="{{ url('ui/frontend_assets') }}/css/icofont.min.css">
-	<link rel="stylesheet" href="{{ url('ui/frontend_assets') }}/css/style.css">
+	<link rel="stylesheet" href="{{ url('ui/frontend_assets/css/bootstrap.min.css') }}">
+	<link rel="stylesheet" href="{{ url('ui/frontend_assets/css/tiny-slider.css') }}">
+	<link rel="stylesheet" href="{{ url('ui/frontend_assets/css/meanmenu.css') }}">
+	<link rel="stylesheet" href="{{ url('ui/frontend_assets/css/fontawesome.min.css') }}">
+	<link rel="stylesheet" href="{{ url('ui/frontend_assets/css/icofont.min.css') }}">
+	<link rel="stylesheet" href="{{ url('ui/frontend_assets/css/style.css') }}">
 </head>
 
 <body>
@@ -48,19 +48,19 @@
 	<!-- ==================== Scripts ==================== -->
 	<!------------------------------------------------------->
 
-	<!-- <script src="assets/js/vendor/jquery.slim.min.js"></script> -->
-	<!-- <script src="assets/js/vendor/jquery-3.5.1.js"></script> -->
-	<script src="{{ url('ui/frontend_assets') }}/js/vendor/jquery.js"></script>
+	{{-- <script src="{{ url('ui/frontend_assets') }}/js/vendor/jquery.slim.min.js"></script> --}}
+	{{-- <script src="{{ url('ui/frontend_assets') }}/js/vendor/jquery-3.5.1.js"></script> --}}
+	<script src="{{ url('ui/frontend_assets/js/vendor/jquery.js') }}"></script>
 
-	<script src="{{ url('ui/frontend_assets') }}/js/vendor/jquery.meanmenu.js"></script>
-	<script src="{{ url('ui/frontend_assets') }}/bootstrap.bundle.min.js"></script>
-	<script src="{{ url('ui/frontend_assets') }}/smooth-scroll.polyfills.min.js"></script>
-	<script src="{{ url('ui/frontend_assets') }}/js/vendor/tiny-slider.js"></script>
-	<script src="{{ url('ui/frontend_assets') }}/js/vendor/lightgallery.min.js"></script>
-	<script src="{{ url('ui/frontend_assets') }}/js/vendor/lg-video.min.js"></script>
+	<script src="{{ url('ui/frontend_assets/js/vendor/jquery.meanmenu.js') }}"></script>
+	<script src="{{ url('ui/frontend_assets/bootstrap.bundle.min.js') }}"></script>
+	<script src="{{ url('ui/frontend_assets/smooth-scroll.polyfills.min.js') }}"></script>
+	<script src="{{ url('ui/frontend_assets/js/vendor/tiny-slider.js') }}"></script>
+	<script src="{{ url('ui/frontend_assets/js/vendor/lightgallery.min.js') }}"></script>
+	<script src="{{ url('ui/frontend_assets/js/vendor/lg-video.min.js') }}"></script>
 
 	<!-- Main script-->
-	<script src="{{ url('ui/frontend_assets') }}/js/main-script.js"></script>
+	<script src="{{ url('ui/frontend_assets/js/main-script.js') }}"></script>
 
 </body>
 
---
+++ resources/views/terms/terms_create.blade.php
@@ -0,0 +1,68 @@
+@extends('ui.admin_panel.master')
+
+@section('title', 'Appraisal Category')
+
+@section('style')
+@endsection
+
+@section('content_title')
+    <h4 class="mt-2">Terms Create</h4>
+@endsection
+
+@section('main_content')
+
+<div class="row page-content">
+    <div class="container">
+            <form action="#" method="post">
+        @csrf
+        {{-- card-body start --}}
+        <div class="card card-default">
+            <div class="card-body">
+                <div class="propertyContent">
+                    {{-- <h6>About us</h6> --}}
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="mb-1 row">
+                                <label for="title" class="col-sm-4 col-form-label col-form-label-sm">Title<span class="important_field">*</span></label>
+                                <div class="col-sm-8">
+                                    <input type="text" class="form-control form-control-sm" id="title"
+                                        name="title" value="{{ old('title') }}">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-12">
+                            <div class="mb-1 row">
+                                <label for="sub_title" class="col-sm-4 col-form-label col-form-label-sm">Sub Title<span class="important_field">*</span></label>
+                                <div class="col-sm-8">
+                                    <input type="text" class="form-control form-control-sm" id="sub_title"
+                                        name="sub_title" value="{{ old('sub_title') }}">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-12">
+                            <div class="mb-1 row">
+                                <label for="description" class="col-sm-4 col-form-label col-form-label-sm">Description<span class="important_field">*</span></label>
+                                <div class="col-sm-8">
+                                    <textarea type="text" class="form-control form-control-sm" id="description"
+                                        name="description" value="{{ old('description') }}"></textarea>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="mb-5 ml-3">
+                <button class="btn btn-warning" type="submit">Submit</button>
+                <a class="btn btn-secondary ml-3" href="{{ route('terms.List') }}">Back</a>
+            </div>
+        </div>
+        {{-- card-body end --}}
+        </form>
+    </div>
+</div>
+
+@endsection
+
+@section('script')
+
+@endsection
